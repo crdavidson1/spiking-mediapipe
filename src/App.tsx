97e7c7fc3153520a87c3e64dc2c2c17c590afec8
xiamonder <acldiamond@gmail.com>
2024-03-21 16:53:42 +0000

    installed blazepose

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import CamFeed from './components/CamFeed'
-import {useEffect, useRef} from 'react';
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import CamFeed from "./components/CamFeed";
+import { useEffect, useRef } from "react";
 import Webcam from "react-webcam";
-import {Camera} from "@mediapipe/camera_utils";
-import {FACEMESH_TESSELATION, HAND_CONNECTIONS, Holistic, Results} from '@mediapipe/holistic';
-import './App.css';
-import {drawConnectors, drawLandmarks} from '@mediapipe/drawing_utils'
-
+import { Camera } from "@mediapipe/camera_utils";
+import {
+  FACEMESH_TESSELATION,
+  HAND_CONNECTIONS,
+  Holistic,
+  Results,
+} from "@mediapipe/holistic";
+import "./App.css";
+import { drawConnectors, drawLandmarks } from "@mediapipe/drawing_utils";
+import * as poseDetection from "@tensorflow-models/pose-detection";
+import "@tensorflow/tfjs-core";
+// Register WebGL backend.
+import "@tensorflow/tfjs-backend-webgl";
+import "@mediapipe/pose";
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   const webcamRef = useRef<Webcam>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   const onResults = (results: Results) => {
-    if (!webcamRef.current?.video || !canvasRef.current) return
+    if (!webcamRef.current?.video || !canvasRef.current) return;
     const videoWidth = webcamRef.current.video.videoWidth;
     const videoHeight = webcamRef.current.video.videoHeight;
     canvasRef.current.width = videoWidth;
@@ -25,42 +34,57 @@ function App() {
 
     const canvasElement = canvasRef.current;
     const canvasCtx = canvasElement.getContext("2d");
-    if (canvasCtx == null) throw new Error('Could not get context');
+    if (canvasCtx == null) throw new Error("Could not get context");
     canvasCtx.save();
     canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
 
     // Only overwrite existing pixels.
-    canvasCtx.globalCompositeOperation = 'source-in';
+    canvasCtx.globalCompositeOperation = "source-in";
     canvasCtx.fillRect(0, 0, canvasElement.width, canvasElement.height);
 
     // Only overwrite missing pixels.
-    canvasCtx.globalCompositeOperation = 'destination-atop';
+    canvasCtx.globalCompositeOperation = "destination-atop";
     canvasCtx.drawImage(
-      results.image, 0, 0, canvasElement.width, canvasElement.height);
+      results.image,
+      0,
+      0,
+      canvasElement.width,
+      canvasElement.height
+    );
 
-    canvasCtx.globalCompositeOperation = 'source-over';
+    canvasCtx.globalCompositeOperation = "source-over";
     // drawConnectors(canvasCtx, results.poseLandmarks, POSE_CONNECTIONS,
     //   {color: '#00FF00', lineWidth: 4});
     // drawLandmarks(canvasCtx, results.poseLandmarks,
     //   {color: '#FF0000', lineWidth: 2});
-    drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION,
-      {color: '#C0C0C070', lineWidth: 1});
-    drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS,
-      {color: '#CC0000', lineWidth: 5});
-    drawLandmarks(canvasCtx, results.leftHandLandmarks,
-      {color: '#00FF00', lineWidth: 2});
-    drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS,
-      {color: '#00CC00', lineWidth: 5});
-    drawLandmarks(canvasCtx, results.rightHandLandmarks,
-      {color: '#FF0000', lineWidth: 2});
+    drawConnectors(canvasCtx, results.faceLandmarks, FACEMESH_TESSELATION, {
+      color: "#C0C0C070",
+      lineWidth: 1,
+    });
+    drawConnectors(canvasCtx, results.leftHandLandmarks, HAND_CONNECTIONS, {
+      color: "#CC0000",
+      lineWidth: 5,
+    });
+    drawLandmarks(canvasCtx, results.leftHandLandmarks, {
+      color: "#00FF00",
+      lineWidth: 2,
+    });
+    drawConnectors(canvasCtx, results.rightHandLandmarks, HAND_CONNECTIONS, {
+      color: "#00CC00",
+      lineWidth: 5,
+    });
+    drawLandmarks(canvasCtx, results.rightHandLandmarks, {
+      color: "#FF0000",
+      lineWidth: 2,
+    });
     canvasCtx.restore();
-  }
+  };
 
   useEffect(() => {
     const holistic = new Holistic({
       locateFile: (file: string) => {
         return `https://cdn.jsdelivr.net/npm/@mediapipe/holistic/${file}`;
-      }
+      },
     });
     holistic.setOptions({
       selfieMode: true,
@@ -70,7 +94,7 @@ function App() {
       smoothSegmentation: true,
       refineFaceLandmarks: true,
       minDetectionConfidence: 0.5,
-      minTrackingConfidence: 0.5
+      minTrackingConfidence: 0.5,
     });
     holistic.onResults(onResults);
 
@@ -78,50 +102,50 @@ function App() {
       typeof webcamRef.current !== "undefined" &&
       webcamRef.current !== null
     ) {
-      if (!webcamRef.current?.video) return
+      if (!webcamRef.current?.video) return;
       const camera = new Camera(webcamRef.current.video, {
         onFrame: async () => {
-          if (!webcamRef.current?.video) return
-          await holistic.send({image: webcamRef.current.video});
+          if (!webcamRef.current?.video) return;
+          await holistic.send({ image: webcamRef.current.video });
         },
         width: 640,
         height: 480,
       });
       camera.start();
     }
-  }, [])
+  }, []);
 
   return (
     <>
       <div>
-      <Webcam
-        ref={webcamRef}
-        style={{
-          position: "absolute",
-          marginLeft: "auto",
-          marginRight: "auto",
-          left: 0,
-          right: 0,
-          textAlign: "center",
-          zIndex: 9,
-          width: 1200,
-          height: 800,
-        }}
-      />
-      <canvas
-        ref={canvasRef}
-        style={{
-          position: "absolute",
-          marginLeft: "auto",
-          marginRight: "auto",
-          left: 0,
-          right: 0,
-          textAlign: "center",
-          zIndex: 9,
-          width: 1200,
-          height: 800,
-        }}
-      />
+        <Webcam
+          ref={webcamRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 1200,
+            height: 800,
+          }}
+        />
+        <canvas
+          ref={canvasRef}
+          style={{
+            position: "absolute",
+            marginLeft: "auto",
+            marginRight: "auto",
+            left: 0,
+            right: 0,
+            textAlign: "center",
+            zIndex: 9,
+            width: 1200,
+            height: 800,
+          }}
+        />
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -142,7 +166,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
